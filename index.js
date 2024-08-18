6dae6e2cb688ae97f2de6aa901290699875d0fb9
rupalbohra <rupalbohra4@gmail.com>
2024-08-18 22:13:14 +0530

    changes3

--- index.js
+++ index.js
@@ -17,13 +17,10 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cors());
 
 mongoose
-  .connect(
-    "mongodb+srv://rajatpant215:Zo4qzV6JusNmcioP@cluster0.ktvsphk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log("NOT CONNECTED TO NETWORK", err));
 
